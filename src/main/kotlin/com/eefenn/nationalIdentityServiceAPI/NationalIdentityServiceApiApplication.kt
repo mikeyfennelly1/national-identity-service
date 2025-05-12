39dfed3fff73f167f6687cd339078687c577fdeb
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-05-12 20:22:22 +0100

    spring initalizr

---
+++ src/main/kotlin/com/eefenn/nationalIdentityServiceAPI/NationalIdentityServiceApiApplication.kt
@@ -0,0 +1,11 @@
+package com.eefenn.nationalIdentityServiceAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NationalIdentityServiceApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<NationalIdentityServiceApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/eefenn/nationalIdentityServiceAPI/NationalIdentityServiceApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eefenn.nationalIdentityServiceAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NationalIdentityServiceApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
